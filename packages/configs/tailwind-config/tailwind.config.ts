f00e8fdde2a133c4aa33a2a95cc25c43bf3f9261
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2025-09-03 14:59:21 +0500

    Add sample data into the email portal sidebar

--- packages/configs/tailwind-config/tailwind.config.ts
+++ packages/configs/tailwind-config/tailwind.config.ts
@@ -4,5 +4,6 @@ export default {
         "../../ui/src/components/**/*.{js,ts,jsx,tsx}",
         "../../ui/src/components/**/*.story.{js,ts,jsx,tsx}",
         "./.storybook/**/*.{ts,tsx,js,jsx}"
-    ]
+    ],
+    plugins: [require('tailwind-animate')]
 } satisfies Config;
